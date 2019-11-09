1dae946807dab8cb5cffca826b229b0bede74eae
absinth <absinth84@gmail.com>
2019-11-09 16:53:01 +0100

    added reset all weeklyplan

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # flaskheat
+
+
+#Run Debug mode
+env FLASK_APP=server.py flask run --debugger --reload
+sudo docker run --name redis-flaskheat -v redis:/data -d -p 127.0.0.1:6379:6379 --rm redis
--- server.py
+++ server.py
@@ -4,35 +4,36 @@ import redis_connector
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-
+redisPrefix = "flaskheat"
+days = ['mon','tue','wed','thu','fri','sat','sun']
 
 
 
 @app.route('/')
 def home():
     
-    lastTemp = redis_connector.redisCmdHget('flaskheat:general', 'lastTemp')
-    lastOutTemp = redis_connector.redisCmdHget('flaskheat:general', 'outTemp')
+    lastTemp = redis_connector.redisCmdHget(redisPrefix + ':general', 'lastTemp')
+    lastOutTemp = redis_connector.redisCmdHget(redisPrefix + ':general', 'outTemp')
     return render_template('home.html', lastTemp = lastTemp, lastOutTemp = lastOutTemp)
 
 @app.route('/weeklyplan', methods=['GET', 'POST'])
 def weeklyplan():
-    days = ['mon','tue','wed','thu','fri','sat','sun']
+    
+
     if request.method == 'GET':
         #Get Weeklyplan from redis
         
-        weekly = [['']*24]*7
+        weekly = [[0 for i in range(24)] for i in range(7)]
         #print(weekly)
         i = 0
         for day in days:
             for hour in range(24):
-                result = redis_connector.redisCmdHget('flaskheat:weeklyplan:' + day , hour)
+                result = redis_connector.redisCmdHget(redisPrefix + ':weeklyplan:' + day , hour)
                 weekly[i][hour] = result
-                print(day, i, hour, weekly[i][hour], result)
+                #print(day, i, hour, weekly[i][hour], result)
             #print(weekly[i])
             i = i + 1
         print(weekly)
-        #print(redis_connector.redisCmdHget('flaskheat:weeklyplan:' + 'mon' , '0'))
         return render_template('weeklyplan.html', weekly = weekly)
 
     if request.method == 'POST':
@@ -48,24 +49,36 @@ def weeklyplan():
                     selDay = day
                     selHour = hour
                 
-                print(day, 'formDayHour_' + str(i) + '_' + str(hour))    
+                #print(day, 'formDayHour_' + str(i) + '_' + str(hour))    
             i = i + 1
         #print(selDay, hour)
 
         #save data on Redis
-        #print('flaskheat:weeklyplan:' + selDay, selHour, data['value'])
-        redis_connector.redisCmdHset('flaskheat:weeklyplan:' + selDay, selHour, data['value'])
+        #print(redisPrefix + ':weeklyplan:' + selDay, selHour, data['value'])
+        redis_connector.redisCmdHset(redisPrefix + ':weeklyplan:' + selDay, selHour, data['value'])
         #return "200"
-        return redirect("/weeklyplan", code=302)
+        return redirect("/weeklyplan", code=301)
 
+@app.route('/weeklyplan/reset', methods=['POST'])
+def weeklyplanReset():
+    #Reset weeklyplan to OFF
+    data = request.form
+    print(data)
+    if request.form['delete'] == 'true':
+        print("TRUE")
+        for day in days:   
+            for hour in range(24):
+                redis_connector.redisCmdHset(redisPrefix + ':weeklyplan:' + day, hour, '0')
+
+    return redirect(request.referrer)
 
 
 @app.route('/generalsettings', methods=['GET', 'POST'])
 def generalsettings():
     if request.method == 'GET':
-        enabled = redis_connector.redisCmdHget('flaskheat:general', 'enabled')
-        dayTemp = redis_connector.redisCmdHget('flaskheat:general', 'dayTemp')
-        nightTemp = redis_connector.redisCmdHget('flaskheat:general', 'nightTemp')
+        enabled = redis_connector.redisCmdHget(redisPrefix + ':general', 'enabled')
+        dayTemp = redis_connector.redisCmdHget(redisPrefix + ':general', 'dayTemp')
+        nightTemp = redis_connector.redisCmdHget(redisPrefix + ':general', 'nightTemp')
         if enabled == 'true':
             enabled = 'checked'
         print(enabled, dayTemp, nightTemp)
@@ -78,10 +91,10 @@ def generalsettings():
 
         #Save settings
         try:
-            redis_connector.redisCmdHset('flaskheat:general', 'enabled', request.form['enabled'])
-            redis_connector.redisCmdHset('flaskheat:general', 'dayTemp', request.form['dayTemp'])
-            redis_connector.redisCmdHset('flaskheat:general', 'nightTemp', request.form['nightTemp'])
-            return redirect("/generalsettings", code=302)
+            redis_connector.redisCmdHset(redisPrefix + ':general', 'enabled', request.form['enabled'])
+            redis_connector.redisCmdHset(redisPrefix + ':general', 'dayTemp', request.form['dayTemp'])
+            redis_connector.redisCmdHset(redisPrefix + ':general', 'nightTemp', request.form['nightTemp'])
+            return redirect("/generalsettings", code=301)
 
         except:
             return EnvironmentError
