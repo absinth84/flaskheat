8c88cea37b71a7edbab1da3c1805faca8d64a62f
absinth <absinth84@gmail.com>
2019-11-10 19:16:00 +0100

    fix

--- temp_reader.py
+++ temp_reader.py
@@ -31,10 +31,10 @@ if redis_connector.redisCmdHget(redisPrefix + ':general', 'enableHistoricalData'
 
 if redis_connector.redisCmdHget(redisPrefix + ':general', 'enabled'):
     # Start for maintain temp > min
-    if temperature < float(redis_connector.redisCmdHget(redisPrefix + ':general', 'minTemp') - delta):
+    if temperature < (float(redis_connector.redisCmdHget(redisPrefix + ':general', 'minTemp')) - delta):
         realy = 1
         print("Start")
-    elif temperature > float(redis_connector.redisCmdHget(redisPrefix + ':general', 'minTemp') + delta):
+    elif temperature > (float(redis_connector.redisCmdHget(redisPrefix + ':general', 'minTemp')) + delta):
         #check current weeklyplan configuration
         currentPeriod = datetime.datetime.now()
         currentConfig = redis_connector.redisCmdHget(redisPrefix + ':weeklyplan:' + days[currentPeriod.weekday()] , currentPeriod.hour)
@@ -44,18 +44,18 @@ if redis_connector.redisCmdHget(redisPrefix + ':general', 'enabled'):
             print("Stop")
         #DAY
         elif currentPeriod == 1:
-            if temperature < float(redis_connector.redisCmdHget(redisPrefix + ':general', 'dayTemp') - delta):
+            if temperature < (float(redis_connector.redisCmdHget(redisPrefix + ':general', 'dayTemp')) - delta):
                 realy = 1
                 print("Start")
-            elif temperature > float(redis_connector.redisCmdHget(redisPrefix + ':general', 'dayTemp') + delta):
+            elif temperature > (float(redis_connector.redisCmdHget(redisPrefix + ':general', 'dayTemp')) + delta):
                 realy = 0
                 print("Stop")
         #NIGHT    
         elif currentPeriod == 2:
-            if temperature < float(redis_connector.redisCmdHget(redisPrefix + ':general', 'nightTemp') - delta):
+            if temperature < (float(redis_connector.redisCmdHget(redisPrefix + ':general', 'nightTemp')) - delta):
                 realy = 1
                 print("Start")
-            elif temperature > float(redis_connector.redisCmdHget(redisPrefix + ':general', 'nightTemp') + delta):
+            elif temperature > (float(redis_connector.redisCmdHget(redisPrefix + ':general', 'nightTemp')) + delta):
                 realy = 0
                 print("Stop")
     else:
